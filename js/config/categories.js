16b56823d9eb74516ff2f06ed034197b05e1ac2d
robdor80 <damianr.belmont@gmail.com>
2026-01-31 17:27:25 +0100

    categorias

---
+++ js/config/categories.js
@@ -0,0 +1,52 @@
+/**
+ * Configuración de Categorías y Subcategorías
+ * Permite expansión futura añadiendo nuevos elementos aquí.
+ */
+
+export const Categories = [
+    {
+        id: "vivienda",
+        name: "Vivienda",
+        subcategories: ["Hipoteca", "Comunidad", "Garajes comunidad", "Derramas", "Luz", "Agua", "Gas", "Seguro hogar"]
+    },
+    {
+        id: "alimentacion",
+        name: "Alimentación",
+        subcategories: ["Mercadona", "Carrefour", "Gadis", "Alcampo", "Froiz", "Lidl", "Aldi", "Erosky", "Otro"]
+    },
+    {
+        id: "amazon",
+        name: "Amazon",
+        subcategories: ["General"] // Subcategoría genérica por defecto
+    },
+    {
+        id: "zalando",
+        name: "Zalando",
+        subcategories: ["General"]
+    },
+    {
+        id: "shein",
+        name: "Shein",
+        subcategories: ["General"]
+    },
+    {
+        id: "entretenimiento",
+        name: "Entretenimiento",
+        subcategories: ["Netflix", "Disney", "HBO", "Amazon anual", "DIGI", "Spotify", "Youtube"]
+    },
+    {
+        id: "automoviles",
+        name: "Automóviles",
+        subcategories: ["Gasolina", "Seguro", "Mantenimiento", "Reparación", "ITV", "Plaza 500"]
+    },
+    {
+        id: "parranda",
+        name: "Parranda",
+        subcategories: ["Hoteles", "Restaurantes", "Fiesta"]
+    },
+    {
+        id: "personal",
+        name: "Personal",
+        subcategories: ["Peluquería", "Estética", "Gimnasio"]
+    }
+];
