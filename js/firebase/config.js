1ee787a83501770c5137cab6a7726f8a0d99f72e
robdor80 <damianr.belmont@gmail.com>
2026-01-31 16:27:45 +0100

    firebase ok

---
+++ js/firebase/config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuración de Firebase - Seguridad Nivel 3
+ * Este archivo solo exporta las credenciales.
+ */
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyDxkF0w3zzWOpsZKQ4cPR6cmlm9AH42s",
+  authDomain: "web-gastos-c559a.firebaseapp.com",
+  projectId: "web-gastos-c559a",
+  storageBucket: "web-gastos-c559a.firebasestorage.app",
+  messagingSenderId: "512419216911",
+  appId: "1:512419216911:web:079eabaa1a4afe65c72d7a"
+};
