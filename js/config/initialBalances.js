f53011dca24c85d9e6900eca8e9406241c9cea38
robdor80 <damianr.belmont@gmail.com>
2026-01-31 17:15:42 +0100

    cantidades

---
+++ js/config/initialBalances.js
@@ -0,0 +1,10 @@
+/**
+ * Configuración de Saldos Base
+ * Estos son los importes antes de la nómina de febrero 2026.
+ */
+
+export const InitialBalances = {
+    bbva: 40,      // Sustituye por tu cantidad real
+    ing: 4544.55,     // Sustituye por tu cantidad real
+    pension: 4503.71,    // Sustituye por tu cantidad real
+};
